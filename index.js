3ec639b9b3dfccc0f8235bcfa080e83eeae5df8c
WiiruxCruz <hialf.92@gmail.com>
2022-03-31 00:26:31 -0600

    Configuración de petición get

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hola de neuvo');
+const express = require('express');
+const res = require('express/lib/response');
+
+//Crear el servidor/aplicacion de express
+const app = express();
+
+// GET
+app.get('/', ( req, resp) => {
+	//console.log('Petición en el /');
+
+	resp.status(200).json({
+		ok: true,
+		msg: 'Todo salio bien',
+		uid: 1234
+	});
+});
+
+app.listen( 4000, () => {
+	console.log(`Servidor corriendo en puerto ${4000}`);
+});

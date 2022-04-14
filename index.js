9e6c4032d619dc1b47d81ae698c9643d8e555819
WiiruxCruz <hialf.92@gmail.com>
2022-04-14 01:16:48 -0500

    Configuración al backend para servir los archivos

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ app.use( express.json() );
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 
+//Manejar demas rutas
+app.get('*', (req, resp) => {
+	resp.sendFile( path.resolve( __dirname, 'public/index.html'));
+});
+
 app.listen( process.env.PORT , () => {
 	console.log(`Servidor corriendo en puerto ${4000}`);
 });
